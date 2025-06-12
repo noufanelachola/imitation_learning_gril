58863b9707d7e779eb7e69c96d9b51cf73332187
noufanelachola <noufansadiqelachola@gmail.com>
2025-06-12 09:12:57 +0530

    changes

--- utils.py
+++ utils.py
@@ -1,17 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from batch_loader import generate_gril
 
-def visualise(image: np.ndarray, coord: np.ndarray) -> None:
+
+def visualise(image: np.ndarray, gaze: np.ndarray) -> None:
     """
         shows the image with its gaze point
     """
 
-    coord = coord * [image.shape[0], image.shape[1]]
+    gaze = gaze * [image.shape[0], image.shape[1]]
 
     plt.imshow(image)
-    plt.scatter(coord[0], coord[1], c='red', s=40, label='Gaze')
-    plt.title(f"Gaze at: {coord}")
+    plt.scatter(gaze[0], gaze[1], c='red', s=40, label='Gaze')
+    plt.title(f"Gaze at: {gaze}")
     plt.legend()
     plt.axis('off')
     plt.show()
+
+
+def test_generate_gril(path: str, file_list: list, max: int):
+    """
+        function to test generate_gril() 
+    """
+
+    gen = generate_gril(path, file_list)
+    print(gen)
+
+    for i, (x, y) in enumerate(gen):
+        print("Sample : {i+1}")
+        print(f"Image : {x['image'].shape}")
+        print(f"Depth : {x['depth'].shape}")
+        print(f"Gaze : {y['gaze']}")
+        print(f"Action : {y['action']}")
+        print("-"*15)
+
+        if i == max:
+            break
